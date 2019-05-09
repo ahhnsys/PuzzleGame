57d1d489f647abb1abc275b29c4b60a2fc66fceb
Natalie Phan <u6671390@anu.edu.au>
2019-05-09 21:31:24 +1000

    Added WelcomeActivity and AppActivity changes

--- app/src/main/java/com/example/assignapp2019s1/AppActivity.java
+++ app/src/main/java/com/example/assignapp2019s1/AppActivity.java
@@ -3,11 +3,11 @@ package com.example.assignapp2019s1;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class AppActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_app);
     }
 }
---
+++ app/src/main/java/com/example/assignapp2019s1/WelcomeActivity.java
@@ -0,0 +1,20 @@
+package com.example.assignapp2019s1;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void startButton(View v) {
+        Intent intent = new Intent(getBaseContext(),AppActivity.class);
+        startActivity(intent);
+    }
+}
