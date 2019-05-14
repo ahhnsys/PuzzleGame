5243dbb1087e7551ca3660334fbf07ba4a177087
Yishun Shi <ahhnsys21@gmail.com>
2019-05-14 19:11:32 +1000

    Basically complete the first level

--- app/src/main/java/com/example/assignapp2019s1/puzzles/PuzzleOneActivity.java
+++ app/src/main/java/com/example/assignapp2019s1/puzzles/PuzzleOneActivity.java
@@ -4,6 +4,7 @@ import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.assignapp2019s1.R;
@@ -23,41 +24,49 @@ public class PuzzleOneActivity extends AppCompatActivity {
 
     public void onClickButtonA(View view){
         answer = answer + 'A';
+        updateScreen(findViewById(R.id.answerScreen));
         check();
     }
 
     public void onClickButtonE(View view){
         answer = answer + 'E';
+        updateScreen(findViewById(R.id.answerScreen));
         check();
     }
 
     public void onClickButtonF(View view){
         answer = answer + 'F';
+        updateScreen(findViewById(R.id.answerScreen));
         check();
     }
 
     public void onClickButtonO(View view){
         answer = answer + 'O';
+        updateScreen(findViewById(R.id.answerScreen));
         check();
     }
 
     public void onClickButtonR(View view){
         answer = answer + 'R';
+        updateScreen(findViewById(R.id.answerScreen));
         check();
     }
 
     public void onClickButtonS(View view){
         answer = answer + 'S';
+        updateScreen(findViewById(R.id.answerScreen));
         check();
     }
 
     public void onClickButtonT(View view){
         answer = answer + 'T';
+        updateScreen(findViewById(R.id.answerScreen));
         check();
     }
 
     public void onClickButtonW(View view){
         answer = answer + 'W';
+        updateScreen(findViewById(R.id.answerScreen));
         check();
     }
 
@@ -88,14 +97,25 @@ public class PuzzleOneActivity extends AppCompatActivity {
 
     public void clear(){
         answer = "";
+        updateScreen(findViewById(R.id.answerScreen));
     }
 
     public void onClickButtonBack(View view){
         finish();
     }
 
+    public void onClickButtonHelp(View view){
+        TextView tv = findViewById(R.id.textViewLevelOneHint);
+        tv.setText("Hint: This word is related to the computer");
+    }
+
     public void onClickButtonSkip(View view){
         checkedRightAnswer();
+    }
+
+    public void updateScreen(View view){
+        TextView tv = view.findViewById(R.id.answerScreen);
+        tv.setText(answer);
     }
 
 
